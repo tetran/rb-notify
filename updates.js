9a71a5c237c3af3d4aa7d7f6b7c88b6b5b2ca228
kkoichi <coarse.groung@gmail.com>
2014-04-30 00:58:13 +0900

    working #3

--- updates.js
+++ updates.js
@@ -16,8 +16,60 @@ try {
     url_reviewee = baseurl + '/api/review-requests/?from-user=' + rbRule.username;
 } catch (ignore) {
 }
+/*
+ * TODO
+ */
+var storedHtml_reviewer = localStorage.rbList_to;
+if (storedHtml_reviewer) {
+    localStorage.rbList_to = '';
+    container_reviewer.innerHTML = storedHtml_reviewer;
+    var elems = document.querySelectorAll('#reviewer .review-requests--update');
+    for (var i = 0, li; i < elems.length; i++) {
+        li = elems[i];
+        console.log(li);
+        addClickEvent(li, li.getAttribute('data-storage-key'));
+    }
+}
 
-function update(url, container) {
+/*
+ * TODO
+ */
+var storedHtml_reviewee = localStorage.rbList_from;
+if (storedHtml_reviewee) {
+    localStorage.rbList_from = '';
+    container_reviewee.innerHTML = storedHtml_reviewee;
+    var elems = document.querySelectorAll('#reviewee .review-requests--update');
+    for (var i = 0, li; i < elems.length; i++) {
+        li = elems[i];
+        console.log(li);
+        addClickEvent(li, li.getAttribute('data-storage-key'));
+    }
+}
+
+function addClickEvent(li, storageKey) {
+    li.addEventListener('click', function() {
+        this.classList.add('fade-out');
+        localStorage[storageKey] = 'watched';
+        var thisUrl = baseurl+req.url,
+            self = this;
+        setTimeout(function() {
+            chrome.tabs.getAllInWindow(undefined, function(tabs) {
+                for (var i = 0, len = tabs.length; i < len; i++) {
+                    var tab = tabs[i];
+                    if (tab.url && tab.url.indexOf(thisUrl) == 0) {
+                        chrome.tabs.update(tab.id, {selected: true});
+                        return;
+                    }
+                }
+                // could not find review board tab.
+                chrome.tabs.create({url: thisUrl});
+            });
+            self.parentNode.removeChild(self);
+        }, 400);
+    }, false);
+}
+
+function update(url, container, type) {
     if (!rbRule) return;
 
     var xhr = new XMLHttpRequest();
@@ -29,43 +81,27 @@ function update(url, container) {
         xhr.onreadystatechange = function() {
             if (xhr.readyState != 4) return;
             if (xhr.status == 200) {
+                var allHtml = '';
                 JSON.parse(xhr.responseText).review_requests.forEach(function(req) {
                     console.log(req);
-                    var storageKey = url + '-' + req.id + '-' + req.last_updated;
-                    if (localStorage[storageKey] === 'watched') {
+                    var storageKey = type + '-' + req.id + '-' + req.last_updated;
+                    if (localStorage[storageKey] === 'fetched') {
                         return;
                     }
                     localStorage[storageKey] = 'fetched';
 
                     var li = document.createElement('li');
                     li.className = 'review-requests--update';
-                    li.setAttribute('data-review-url', req.url);
+                    li.setAttribute('data-storage-key', storageKey);
                     li.innerHTML = Mustache.render(listTemplate, {
                         summary: req.summary,
                         time: req.last_updated
                     });
-                    li.addEventListener('click', function() {
-                        this.classList.add('fade-out');
-                        localStorage[storageKey] = 'watched';
-                        var thisUrl = baseurl+req.url,
-                            self = this;
-                        setTimeout(function() {
-                            chrome.tabs.getAllInWindow(undefined, function(tabs) {
-                                for (var i = 0, len = tabs.length; i < len; i++) {
-                                    var tab = tabs[i];
-                                    if (tab.url && tab.url.indexOf(thisUrl) == 0) {
-                                        chrome.tabs.update(tab.id, {selected: true});
-                                        return;
-                                    }
-                                }
-                                // could not find review board tab.
-                                chrome.tabs.create({url: thisUrl});
-                            });
-                            self.parentNode.removeChild(self);
-                        }, 400);
-                    }, false);
+                    addClickEvent(li, storageKey);
                     container.appendChild(li);
+                    allHtml += li.outerHTML;
                 });
+                localStorage['rbList_' + type] = allHtml;
             }
             window.clearTimeout(abortTimerId);
         };
@@ -80,10 +116,10 @@ function clear() {
     container_reviewee.innerHTML = '';
 }
 function updateAsReviewer() {
-    update(url_reviewer, container_reviewer);
+    update(url_reviewer, container_reviewer, 'to');
 }
 function updateAsReviewee() {
-    update(url_reviewee, container_reviewee);
+    update(url_reviewee, container_reviewee, 'from');
 }
 function updateDate() {
     lastUpdatedDate = new Date();
